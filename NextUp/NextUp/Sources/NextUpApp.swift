634168986399ec319943cee82856bebd3146a2a0
Nate Koch <nkoch@jaaku.xyz>
2021-08-02 09:54:18 -0700

    added core data entities and folders and moved stuff around for organization

---
+++ NextUp/NextUp/Sources/NextUpApp.swift
@@ -0,0 +1,20 @@
+//
+//  NextUpApp.swift
+//  NextUp
+//
+//  Created by Nate Koch on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct NextUpApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
